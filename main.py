1a4b83ea029c6acc06cd666ebf10b2dfe6005607
Aslon <70798072+aslon1213@users.noreply.github.com>
2025-06-17 17:30:15 +0500

    Revert "images dir created when not exists (#3)"
    This reverts commit 081215ca277df4c311d912b5df45855cb36bd8db.

--- main.py
+++ main.py
@@ -6,19 +6,12 @@ import time
 from functools import wraps
 from uuid import uuid4
 from typing import List
-import os
-#app = FastAPI(docs_url="/image/classification/docs")
-app = FastAPI(
-    root_path="/image/classification/docs",
-    docs_url="/",  # This will make docs available at /image/classification/docs/
-    redoc_url=None,  # Or set to another if needed
-    openapi_url="/openapi.json"
-)
+app = FastAPI(docs_url="/image/classification/docs")
+
 config = Config()
 
 classifier = ImageClassifier()
-if not os.path.exists("images"):
-    os.makedirs("images")
+
 def timer(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
@@ -70,7 +63,7 @@ async def predict_image(image: UploadFile = File(...), n_results: int = 5):
 
 if not config.use_auth:
     logger.info("No authentication")
-    @app.post("/predict")
+    @app.post("/image/classification/predict")
     async def predict(image: UploadFile = File(...), n_results: int = Query(default=5)):
         return await predict_image(image, n_results)
 else:
@@ -80,7 +73,7 @@ else:
         return await predict_image(image, n_results)
 
 
-@app.post("/upload_classes")
+@app.post("/image/classification/upload_classes")
 def upload_classes(classes: List[str]):
     with open("categories_list_new.txt", "w") as f:
         for class_ in classes:
@@ -89,6 +82,6 @@ def upload_classes(classes: List[str]):
     classifier.load_classes("categories_list_new.txt")
     return {"message": "Classes uploaded successfully"}
 
-@app.get("/classes")
+@app.get("/image/classification/classes")
 def get_classes():
     return {"classes": classifier.classes}
