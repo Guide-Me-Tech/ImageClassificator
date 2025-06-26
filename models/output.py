0e3355ac93ab39374d4720a7615dd8ba6bded86c
Doston <roostamovic@yahoo.com>
2025-06-26 14:05:05 +0500

    Changed code logic to comprise with Qdrant (#8)
    * Changed code logic to comprise with Qdrant
    
    * Edited Similarity Search code for deployment
    
    * Final changes
    
    ---------
    
    Co-authored-by: aslon <hamidovaslon1@gmail.com>

--- models/output.py
+++ models/output.py
@@ -8,14 +8,22 @@ from typing import List, Dict
 class ClassPrediction(BaseModel):
     class_name: str
     confidence: float
+    idx: int
 
+class ClassSimilarity(BaseModel):
+    sim_score: float
+    product_name: str
 
 class Prediction(BaseModel):
     classes_en: List[ClassPrediction] = []
     classes_uz: List[ClassPrediction] = []
     classes_ru: List[ClassPrediction] = []
 
+class Similarity(BaseModel):
+    similar_products: List[ClassSimilarity] = []
+    
 class Output(BaseModel):
     prediction: Prediction
+    similarity: Similarity
     error: Dict[str, str]
     
