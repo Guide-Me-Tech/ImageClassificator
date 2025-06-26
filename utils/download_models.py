57d5fb6866452c593057c67d85fd7874e911c676
Aslon <70798072+aslon1213@users.noreply.github.com>
2025-06-26 16:54:14 +0500

    Hotfix/2 (#11)
    * merge conflict merge
    
    * load tests and some data changes

--- utils/download_models.py
+++ utils/download_models.py
@@ -2,5 +2,7 @@
 import torch
 import clip
 import os
+import open_clip
 device = "cuda" if torch.cuda.is_available() else "cpu"
 clip.load(os.getenv("MODEL_NAME"), device)
+open_clip.get_tokenizer(os.getenv("MODEL_NAME_OPEN_CLIP") or "ViT-B-32-quickgelu")
